b138dec6617a5922f84fa87b415883458cb03ca9
mzkki <08harismu@gmail.com>
2024-12-05 01:21:43 +0800

    update swagger servers

--- CC/utils/swagger.js
+++ CC/utils/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://hastravani-api-693713901451.asia-southeast2.run.app',
       },
     ],
   },
